dd0e293db8526d2e73f9e42bb65c75eff49d4604
Josh Akeman <joshakeman@gmail.com>
2019-06-10 14:55:30 -0400

    finished tutorial, displaying three cards from contentful

---
+++ material-ui/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NavBar from './components/NavBar'
+import CourseList from './components/CourseList'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <CourseList />
+    </div>
+  );
+}
+
+export default App;
